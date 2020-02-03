d33104f3d4dd338ea2fa619c1b21173f73d559c9
superwwc <60102627+superwwc@users.noreply.github.com>
2020-02-03 18:22:16 -0500

    Update output.php

--- controlador/output.php
+++ controlador/output.php
@@ -3,6 +3,6 @@
 //OUTPUT
 	echo "{<br>&nbsp;transactions:<br>&nbsp;&nbsp;[<br>";
 		repite($final,sizeof($final),0);//imprimo las transacciones realizadas
-		remanente($matriz,$n);
+		remanente($matriz,$n);//imprimo si hay remains
 	echo "<br>&nbsp;&nbsp;]<br>}<br>";
 ?>
